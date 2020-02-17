0f8d536e3885b45d5447f1373a3eee46924d76d8
Tadeo Ramirez Parada <tadeoramirezp@gmail.com>
2020-02-16 20:46:44 -0800

    Complete version of clim/pheno merger script

--- R Scripts/Merge herbarium and climate data.R
+++ R Scripts/Merge herbarium and climate data.R
@@ -1,11 +1,11 @@
 library(raster)
-
+library(lubridate)
 
 # Import phenologicla data
 dat_pheno <- read.csv("./Data/herbarium/occurrences.csv")
 
-# Keep only georeferenced records
-dat_pheno <-  dat_pheno[complete.cases(dat_pheno[,c("decimalLatitude","decimalLongitude")]), ]
+# Keep only georeferenced records with DOY and YEAR
+dat_pheno <-  dat_pheno[complete.cases(dat_pheno[,c("decimalLatitude","decimalLongitude","startDayOfYear","year")]), ]
 
 
 #
@@ -18,7 +18,7 @@ dat_pheno <-  dat_pheno[complete.cases(dat_pheno[,c("decimalLatitude","decimalLo
 
 
 
-var_seasonal <- c("springtmean","winterppt")
+var_seasonal <- c("winterppt","wintertmin","wintertmean","wintertmax","springppt","springtmin","springtmean","springtmax")
 
 
 # Loop through each seasonal variable
@@ -101,11 +101,119 @@ for(var in var_annual){
 }
 
 
-# Check that values make sense
-View(dat_pheno[,c("year","tmax_annual","tmean_annual","tmin_annual","springtmean","winterppt")])
 
 
 
+
+
+#
+# MERGE VARIABLES IN MONTH OF COLLECTION (MOC)
+#
+
+
+# Create lists to match DOY with a month for leap and non-leap years
+month_leap <- data.frame(matrix(NA, nrow=366, ncol = 2))
+month_nonleap <- data.frame(matrix(NA, nrow=365, ncol = 2))
+
+colnames(month_leap) = colnames(month_nonleap) = c("DOY","month")
+
+month_leap$DOY <- 1:366
+month_nonleap$DOY <- 1:365
+
+
+month_leap$month <-    c(rep("01", 31),rep("02", 29),rep("03", 31),
+                         rep("04", 30),rep("05", 31),rep("06", 30),
+                         rep("07", 31),rep("08", 31),rep("09", 30),
+                         rep("10", 31),rep("11", 30),rep("12", 31))
+
+month_nonleap$month <-    c(rep("01", 31),rep("02", 28),rep("03", 31),
+                             rep("04", 30),rep("05", 31),rep("06", 30),
+                             rep("07", 31),rep("08", 31),rep("09", 30),
+                             rep("10", 31),rep("11", 30),rep("12", 31))
+
+
+
+
+
+
+
+
+var_monthly <- c("ppt","tmin","tmax","tmean")
+
+for(var in var_monthly){
+  
+  if(var == "ppt"){
+    
+ # Create list of raster folders and files
+ # data frame
+ raster_list <- data.frame(matrix(NA, nrow = length(1895:2014)*12, ncol = 4))
+ # Fill in years
+ raster_list[,1] <- rep(1895:2014,each=12)
+ # Fill in months
+ raster_list[,2] <- rep(c("01","02","03","04","05","06","07","08","09","10","11","12"), length(1895:2014))
+ 
+ # Fill in folder names
+ 
+ # 1895:1980: M2
+ raster_list[1:max(which(raster_list[,1]==1980)),3] <- 
+   
+   paste("PRISM_",var,"_stable_4kmM2_",
+          as.vector(t(outer(1895:1980, c("01","02","03","04","05","06","07","08","09","10","11","12"), paste, sep=""))),
+         "_bil",sep = "")
+
+ # 1981:2014: M3
+ raster_list[min(which(raster_list[,1]==1981)):nrow(raster_list),3] <-
+   
+   paste("PRISM_",var,"_stable_4kmM3_",
+         as.vector(t(outer(1981:2014, c("01","02","03","04","05","06","07","08","09","10","11","12"), paste, sep=""))),
+         "_bil",sep = "")
+ 
+ # Fill in raster file names
+ raster_list[,4] <- paste(raster_list[,3],".bil",sep="")
+ 
+ colnames(raster_list) <- c("year","month","folder","file_name")
+  
+  } 
+  
+  else { 
+    raster_list <- data.frame(matrix(NA, nrow = length(1895:2014)*12, ncol = 4))
+    raster_list[,1] <- rep(1895:2014,each=12)
+    raster_list[,2] <- rep(c("01","02","03","04","05","06","07","08","09","10","11","12"), length(1895:2014))
+    raster_list[,3] <- 
+      
+      paste("PRISM_",var,"_stable_4kmM3_",
+            as.vector(t(outer(1895:2014, c("01","02","03","04","05","06","07","08","09","10","11","12"), paste, sep=""))),
+            "_bil",sep = "")
+
+    raster_list[,4] <- paste(raster_list[,3],".bil",sep="")
+    
+    colnames(raster_list) <- c("year","month","folder","file_name")
+    
+    }
+  
+  
+
+for(i in 1:nrow(dat_pheno)){
+  
+  if(leap_year(dat_pheno[i,"year"])==TRUE){month_i <- month_leap[month_leap$DOY==dat_pheno[i,"startDayOfYear"], "month"]} 
+  
+  else{month_i <- month_nonleap[month_nonleap$DOY==dat_pheno[i,"startDayOfYear"], "month"]}
+  
+  if(dat_pheno[i,"year"]>2014|dat_pheno[i,"year"]<1895) {next}
+  
+  raster_var_i <- raster(paste(getwd(),"/Data/","prism_data/","monthly/", var,"/",
+                               raster_list[raster_list$year==dat_pheno[i,"year"] & raster_list$month == month_i,"folder"],"/",
+                               raster_list[raster_list$year==dat_pheno[i,"year"] & raster_list$month == month_i,"file_name"], sep = ""))
+  
+  
+  dat_pheno[i,paste(var,"MOC",sep = "_")] <- extract(x=raster_var_i, y= dat_pheno[i,c("decimalLongitude","decimalLatitude")])
+  
+}
+
+}
+
+View(dat_pheno[,c("year","startDayOfYear",var_seasonal, "ppt_annual","tmin_annual","tmean_annual","tmax_annual",
+                  "ppt_MOC","tmin_MOC","tmean_MOC","tmax_MOC")])
 
 
 
